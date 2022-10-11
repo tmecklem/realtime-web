9e5f77cafe4cf07f5054491575849f29ae6ac39a
Tim Mecklem <timothy@mecklem.com>
2022-10-10 21:08:34 -0400

    Update phoenix and liveview

--- mix.exs
+++ mix.exs
@@ -33,15 +33,14 @@ defmodule Realtime.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.6.12"},
+      {:phoenix, "~> 1.6.14"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.6"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 3.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 0.17.5"},
+      {:phoenix_live_view, "~> 0.18"},
       {:floki, ">= 0.30.0", only: :test},
-      {:phoenix_live_dashboard, "~> 0.6"},
       {:phoenix_copy, "~> 0.1.1"},
       {:esbuild, "~> 0.4", runtime: Mix.env() == :dev},
       {:swoosh, "~> 1.3"},
