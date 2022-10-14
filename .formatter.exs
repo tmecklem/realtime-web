3a62510364be8e798f586838ff9889d788db8e82
Tim Mecklem <timothy@mecklem.com>
2022-10-13 22:13:56 -0400

    Allow lines to be 120

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,6 @@
 [
   import_deps: [:ecto, :phoenix, :typed_struct],
   inputs: ["*.{ex,exs}", "priv/*/seeds.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  line_length: 120,
   subdirectories: ["priv/*/migrations"]
 ]
