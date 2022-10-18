c97e3bb128f92b112635236406c2e185208c902f
Tim Mecklem <timothy@mecklem.com>
2022-10-17 20:20:55 -0400

    Refactor to filter by relevancy

--- lib/realtime_web/live/better_post_live/index.ex
+++ lib/realtime_web/live/better_post_live/index.ex
@@ -6,6 +6,8 @@ defmodule RealtimeWeb.BetterPostLive.Index do
   alias Realtime.Social.Post
   alias Realtime.Social.PostEvents
 
+  @high_relevancy_threshold 90
+
   @impl true
   def mount(_params, _session, socket) do
     {:ok, socket}
@@ -15,102 +17,77 @@ defmodule RealtimeWeb.BetterPostLive.Index do
   def handle_params(_params, _, socket) do
     if connected?(socket), do: PostEvents.listen_for_events(:post_created)
 
-    :timer.send_after(500, :tick)
+    posts = Social.list_posts()
 
     {
       :noreply,
       socket
-      |> assign(:posts, Social.list_posts())
+      |> assign(:posts, posts)
+      |> assign(:display_posts, posts)
       |> assign(:queued_posts, [])
       |> assign(:last_refreshed, timestamp())
-      |> assign(humanized_last_refreshed: humanized_last_refreshed(timestamp()))
+      |> assign(:high_relevancy, false)
     }
+  end
+
+  @impl true
+  def handle_event("toggle_relevancy", values, socket) do
+    high_relevancy =
+      case values do
+        %{"value" => "on"} -> true
+        _ -> false
+      end
+
+    socket = assign(socket, high_relevancy: high_relevancy)
+    {:noreply, process_queued_posts(socket)}
   end
 
   @impl true
   def handle_event("refresh_posts", _values, socket) do
-    new_posts = Enum.take(socket.assigns.queued_posts ++ socket.assigns.posts, 100)
-
-    {
-      :noreply,
-      socket
-      |> assign(posts: new_posts)
-      |> assign(queued_posts: [])
-      |> assign(last_refreshed: timestamp())
-    }
+    {:noreply, process_queued_posts(socket)}
   end
 
   @impl true
   def handle_info({:post_created, %Post{} = post}, socket) do
-    cond do
-      length(socket.assigns.queued_posts) > 0 ->
-        {
-          :noreply,
-          socket
-          |> assign(queued_posts: [post | socket.assigns.queued_posts])
-        }
+    if socket.assigns.high_relevancy ||
+         (length(socket.assigns.queued_posts) == 0 &&
+            Timex.diff(timestamp(), socket.assigns.last_refreshed, :seconds) > 4) do
+      new_posts = [post | socket.assigns.posts]
 
-      Timex.diff(timestamp(), socket.assigns.last_refreshed, :seconds) > 4 ->
-        {
-          :noreply,
-          socket
-          |> assign(posts: Enum.take([post | socket.assigns.posts], 100))
-          |> assign(last_refreshed: timestamp())
-          |> assign(humanized_last_refreshed: humanized_last_refreshed(timestamp()))
-        }
-
-      true ->
-        {
-          :noreply,
-          socket
-          |> assign(queued_posts: [post | socket.assigns.queued_posts])
-        }
+      {
+        :noreply,
+        socket
+        |> assign(posts: new_posts)
+        |> assign(display_posts: Enum.take(filter_posts(new_posts, socket.assigns.high_relevancy), 100))
+        |> assign(last_refreshed: timestamp())
+      }
+    else
+      {
+        :noreply,
+        socket
+        |> assign(queued_posts: [post | socket.assigns.queued_posts])
+      }
     end
   end
 
-  @impl true
-  def handle_info(:tick, socket) do
-    :timer.send_after(1000, :tick)
+  defp process_queued_posts(socket) do
+    new_posts = Enum.take(socket.assigns.queued_posts ++ socket.assigns.posts, 100)
 
-    socket =
-      cond do
-        length(socket.assigns.queued_posts) > 2 ->
-          socket
-
-        Timex.diff(timestamp(), socket.assigns.last_refreshed, :seconds) > 4 ->
-          new_posts = Enum.take(socket.assigns.queued_posts ++ socket.assigns.posts, 100)
-
-          socket
-          |> assign(posts: new_posts)
-          |> assign(queued_posts: [])
-          |> assign(last_refreshed: timestamp())
-
-        true ->
-          socket
-      end
-
-    {
-      :noreply,
-      socket
-      |> assign(humanized_last_refreshed: humanized_last_refreshed(socket.assigns.last_refreshed))
-    }
+    socket
+    |> assign(posts: new_posts)
+    |> assign(queued_posts: [])
+    |> assign(last_refreshed: timestamp())
+    |> assign(display_posts: Enum.take(filter_posts(socket.assigns.posts, socket.assigns.high_relevancy), 100))
   end
+
+  defp filter_posts(posts, apply_high_threshold) do
+    Enum.filter(posts, &filter_post(&1, if(apply_high_threshold, do: @high_relevancy_threshold, else: 0)))
+  end
+
+  defp filter_post(%Post{relevance: relevance}, threshold) when relevance >= threshold, do: true
+  defp filter_post(_, _), do: false
 
   def timestamp do
     DateTime.now!("America/New_York")
-  end
-
-  defp humanized_last_refreshed(last_refreshed) do
-    timestamp()
-    |> Timex.diff(last_refreshed, :seconds)
-    |> case do
-      time when time < 1 ->
-        "0 seconds"
-
-      time ->
-        time
-        |> Duration.from_seconds()
-        |> Timex.format_duration(:humanized)
-    end
   end
 end
